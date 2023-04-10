b50da5bf0069299f31bb90c23321cad906edd274
axelfrache <axelfrache@gmail.com>
2023-04-10 13:54:13 +0200

    Test formulaire mail depuis github.io

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const forms = document.getElementsByClassName("needs-validation");
+
+    Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener("submit", function (event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+        }, false);
+    });
+});
