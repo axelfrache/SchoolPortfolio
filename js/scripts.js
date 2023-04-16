738d8f7444121d54ad3ea47f44aa92c1419c338e
axelfrache <axelfrache@gmail.com>
2023-04-16 17:13:52 +0200

    Correction eventlistener

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-let currentIndex = 0;
-const carouselImages = document.querySelectorAll(".carousel-image");
-
-function changeImage(direction) {
-    const newIndex = currentIndex + direction;
-
-    if (newIndex < 0 || newIndex >= carouselImages.length) {
-        // Ne change pas d'image si l'utilisateur a atteint la première ou la dernière image
-        return;
-    }
-
-    carouselImages[currentIndex].style.display = "none";
-    currentIndex = newIndex;
-    carouselImages[currentIndex].style.display = "block";
-}
 document.addEventListener("DOMContentLoaded", function () {
     const welcomeBanner = document.querySelector("#welcome-banner");
     const trail = document.querySelector("#trail");
