3ed43acfd0dc83adc8971517854e7e10ffafe182
Axel Frache <axel.frache-ext@agysoft.fr>
2023-09-29 14:47:48 +0200

    Ajout d'un dark mode 🌙

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const welcomeBanner = document.querySelector("#welcome-banner");
     const trail = document.querySelector("#trail");
+    const themeSwitch = document.getElementById('themeSwitch');
+    const forms = document.getElementsByClassName("needs-validation");
 
+    // Pour la bannière de bienvenue
     if (trail) {
         welcomeBanner.addEventListener("mousemove", (event) => {
             const x = event.clientX;
@@ -9,11 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
             trail.style.transform = `translate(${x - 10}px, ${y - 10}px)`;
         });
     }
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-    const forms = document.getElementsByClassName("needs-validation");
-
+    // Pour la validation des formulaires
     Array.prototype.filter.call(forms, function (form) {
         form.addEventListener("submit", function (event) {
             if (form.checkValidity() === false) {
@@ -22,5 +22,18 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             form.classList.add("was-validated");
         }, false);
+    });
+
+    // Pour le switch de mode
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+        themeSwitch.checked = true;
+    }
+
+    themeSwitch.addEventListener('change', () => {
+        document.body.classList.toggle('dark-mode');
+        const theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+        localStorage.setItem('theme', theme);
     });
 });
